6ce73b59da59523c625fde80f5d3f35ca3b44fe0
wangxian <xian366@126.com>
2023-11-30 14:19:55 +0800

    Update main.py
    change BAAI/bge-large-zh-v1.5

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ logging.basicConfig(level=logging.INFO)
 logging.getLogger(__name__)
 
 app = Flask(__name__)
-model = SentenceTransformer('BAAI/bge-base-zh')
+model = SentenceTransformer('BAAI/bge-large-zh-v1.5')
 
 def make_embeddings(text_string):
     sentences = [text_string]
